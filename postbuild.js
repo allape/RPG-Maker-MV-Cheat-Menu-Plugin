3756ecb146998d9e124af82c41361bf3dd5f6dde
AllenSnape <allensnape@gmail.com>
2022-01-11 20:56:16 +0800

    feat: auto copy file config

--- postbuild.js
+++ postbuild.js
@@ -1,2 +1,16 @@
 const fs = require('fs')
+const path = require('path')
 fs.copyFileSync('./plugins_patch.txt', './dist/plugins_patch.txt')
+
+const copycat = path.join(__dirname, 'copycat.json')
+if (fs.existsSync(copycat)) {
+    const thingsToCopy = require(copycat)
+    Object.keys(thingsToCopy).forEach(source => {
+        const sourceFilePath = path.join(__dirname, source)
+        if (fs.existsSync(sourceFilePath)) {
+            const destDir = path.dirname(thingsToCopy[source])
+            if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true })
+            fs.copyFileSync(sourceFilePath, thingsToCopy[source])
+        }
+    })
+}
