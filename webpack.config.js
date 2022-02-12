6d7d49b017bdddb277b79b87a42c9c3c4bf4fe6e
AllenSnape <allensnape@gmail.com>
2022-02-13 01:58:19 +0800

    refactor: babel-ize

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,31 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './build/index.js',
+  entry: './src/index.ts',
   devtool: 'source-map',
-  output: {
-    filename: 'AsCheater.js',
-    path: path.resolve(__dirname, 'dist/www/js/plugins'),
-  },
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript']
+          }
+        }
+      },
       {
         test: /\.(s?[ac]ss)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  output: {
+    filename: 'AsCheater.js',
+    path: path.resolve(__dirname, 'dist/www/js/plugins'),
   },
 }
