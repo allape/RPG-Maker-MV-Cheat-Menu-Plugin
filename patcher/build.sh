c788accc598cae40b234d7f6fbfabe6dc0b871e1
as <allensnape@qq.com>
2022-02-22 15:41:34 +0800

    refactor: refine

--- patcher/build.sh
+++ patcher/build.sh
@@ -3,7 +3,7 @@
 FILENAME=${1:-"patcher"}
 
 # MacOS build
-GOOS=darwin GOARCH=amd64 go build -o "../dist/$FILENAME" .
+#GOOS=darwin GOARCH=amd64 go build -o "../dist/$FILENAME" .
 
 # Windows build
 GOOS=windows GOARCH=amd64 go build -o "../dist/$FILENAME.exe" .
